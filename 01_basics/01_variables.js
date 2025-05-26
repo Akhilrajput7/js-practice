a35e645d7a8b816b24826294ee7a9accd2225a8b
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-05-26 16:01:49 +0000

    js variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14314;
+let accountEmail = "rajputakhil393@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+
+let accountState;
+
+// accountId = 2; // not allowed to changes 
+
+accountEmail = "akhil473@gmail.com";
+accountPassword = "46748"
+accountCity = "delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail ,accountPassword ,accountCity ,accountState])
