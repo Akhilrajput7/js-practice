6cd2eb81989d53508f20333e688e631fe622c60e
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-05-26 17:05:38 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using node js not browser alert is basically used in browser engine like V8 etc
+
+console.log(3+3)
+
+ console.log("Akhil")
+
+
+ let name ="Akhil" // string
+ let age = 18 // number => 2 to power 53 limitation
+ let isLoggedIn = true || false  // boolean
+ let state  = null //  null is a dataype and standlone value // also the type of null is object
+ let value;// undefined
+ //  symbol when we talk about unique value
+ //   object
+
+console.log(typeof null);
+
+
