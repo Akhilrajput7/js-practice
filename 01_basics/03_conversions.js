b838a96e039e2a8bc5b744a3c64c1e4cc14fb4b9
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-05-29 15:10:32 +0000

    changes

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -1,23 +1,67 @@
 let score = "33abc"
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueNumber = Number(score)
 
-console.log(typeof(valueNumber))
-console.log(valueNumber);
+// console.log(typeof(valueNumber))
+// console.log(valueNumber);
 
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 let sum = 333
 
 let stringNumber = String(sum)
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof(stringNumber))
+// console.log(typeof(stringNumber))
 
+// ***********operations ***********
+
+let value = 2
+ let negbvalue = -value
+
+//  console.log(value); 
+//  console.log(negbvalue)
+
+//  console.log(2+2);
+//   console.log(2-2);
+//    console.log(2*2);
+//     console.log(2**2);
+//      console.log(2/2);
+//      console.log(2%2); 
+
+ let str1 = "Hello"
+  let str2 = " akhil"
+
+  let str3 = str1 + str2 
+//   console.log(str3);
+
+//   console.log("1" + 2)
+//   console.log(1 + "2")
+//   console.log("1" + "2")
+//   console.log("1" + 2 + 2)
+//   console.log( 1 + 2 + "2")
+
+//   console.log( (3 + 4) * 5 % 3)
+
+// console.log( true);
+// console.log( +"");
+
+let num1 , num2 , num3 
+
+num1 = num2 = num3 =  2 + 2
+console.log(num1)
+
+let gameCounter  = 100
+gameCounter++
+console.log( "gameCounter" ,gameCounter);
+
+
+
+  
