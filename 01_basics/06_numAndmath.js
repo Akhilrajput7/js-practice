509ac7cba1bedf2fd170843147eef2807e2df138
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-05-31 17:22:03 +0000

    numbers and maths

---
+++ 01_basics/06_numAndmath.js
@@ -0,0 +1,39 @@
+const score =  77
+console.log(score)
+
+const balance = new Number(288.88)
+console.log(balance);
+console.log(typeof(balance))
+
+console.log(balance.toString() +" ".concat("akhil"))
+
+console.log(balance.toFixed(1))
+
+const num = 122.5688
+
+console.log(num.toPrecision(3))
+
+const num1 = 10000000
+console.log(num1.toLocaleString('en-IN'));
+
+// ********** maths *********
+
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.88))
+console.log(Math.sqrt(25))
+console.log(Math.min(1,2,3))
+
+console.log((Math.random()*10)  +1)
+
+
+const min = 10
+const max =20
+
+console.log( Math.floor(Math.random() + (max -min)) + min)
+
+console.log(Math.min(4.55, 5.5 ,34.55))
+
+
+
