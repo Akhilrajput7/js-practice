06509ff421f71139c26597e0e17f3b144f902795
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-06-02 16:38:41 +0000

    arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,50 @@
+// array
+
+const myArray = [ 0,1,2,3,4,5]
+const Heros = ["ironman", "Hulk"]
+const myarr1 = new Array(1,2,3,4,5)
+
+// console.log(myarr[1]);
+
+// methods
+
+// myArray.push(6)
+// myArray.push(115) // push add element in the last index of the array
+// myArray.pop(0)  // pop removes the last value from an array
+
+myArray.unshift(10) //unshift add element in the first index
+myArray.shift() // shift remove element from first index of an array
+
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(4));
+
+// const newArray =  myArray.join() // join method convert the array into the string values
+
+// console.log(myArray);
+// console.log(newArray);
+
+// slice or splice
+
+console.log("A" , myArray);
+
+const myn1  = myArray.slice( 0 , 4)
+
+console.log(myn1);
+console.log("B" , myArray);
+
+const myn2 =  myArray.splice(0,3)
+
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
+
+
