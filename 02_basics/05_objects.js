8d1d95b2cec1c6a84fdbd8363c27e4b53fbe8ba1
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-06-04 17:23:08 +0000

    destructuring

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,17 @@
+// destructuring
+const Course = {
+    CourseName:"js in hindi",
+    price:"999",
+    instructor:"hitesh"
+}
+
+const {instructor:teacher} = Course 
+
+// console.log(instructor);
+console.log(teacher);
+
+// {
+//     name:"akhil",
+//     CourseName:"jscourse",
+//     price:"free"
+// }
