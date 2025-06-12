91b2628f909e4401b2b5cfcc9615992d772de918
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-06-12 15:37:38 +0000

    Truthy and Falsy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,43 @@
+const userEmail = []
+
+
+// if(userEmail){
+//     console.log("got user email");
+    
+// } else {
+//     console.log("dont have email");
+    
+// }
+
+// falsy valuse :- false, 0 ,-0 , "" ,BigInt 0n , null , undefiend , NaN
+// truthy value :- [] , true , 1 , "0" , "false" , " " , {} , function(){}
+
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+    
+// }
+//  const emptyObj ={}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("object is empty"); 
+// }
+
+// Nullish coalescing operator (??) : null undefined 
+
+let val1;
+//  val1 = 5 ?? 10
+//  val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? undefined ?? 15
+
+ console.log(val1);
+
+//  terniary operator
+//  codition ? true : false
+
+const Price = 100
+Price <=80 ? console.log("less than 80") : console.log("more than 80");
+
+
+ 
