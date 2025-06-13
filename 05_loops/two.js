e7749b84617cd3c1a49b1880c3cd3777c775d41e
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-06-13 15:40:06 +0000

    while and do-while loops

---
+++ 05_loops/two.js
@@ -0,0 +1,21 @@
+
+// let i = 0
+// while (i <=10) {
+//     console.log(`value of i is ${i}`)
+//     i = i + 2
+// }
+
+// const myarray = ["ironman" , "caption" , "hulk"]
+// let arr = 0
+// while (arr < myarray.length) {
+//     console.log(`value is ${myarray[arr]}`);
+//     arr++ ;
+// }
+
+// let score  = 1
+
+// do {
+//     console.log(`the socre is ${score}`);
+//         score++
+// } while (score<=10);
+
