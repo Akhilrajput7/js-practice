296d26d5bdcd8ced3293ae68ba8dd0acea445828
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-06-17 03:20:02 +0000

    for in loop

---
+++ 05_loops/four.js
@@ -0,0 +1,33 @@
+const myobj = {
+    js:"javascript",
+    cpp: "c++",
+    rb:"rubby",
+    swift:"swift by apple"
+}
+
+// for in loop
+
+// for (const key in myobj) {
+  
+//     console.log( key , myobj[key]);
+    
+// }
+
+// const programming = ["js" , "java" , "cpp" , "py" ,"rubby"]
+
+// for(const key in programming){
+//     console.log(programming[key]);
+    
+// }
+
+// const map = new Map()
+// map.set('IN' , "india")
+// map.set('USA' , "united states of america")
+// map.set("Fr" , "france")
+// map.set('Gr' , "Germany") 
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
+
