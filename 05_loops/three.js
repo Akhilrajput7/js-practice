10f7805a77e420d674784967422b9afffeec3dc2
Akhil Rajput <149920374+Akhilrajput7@users.noreply.github.com>
2025-06-17 03:17:33 +0000

    For of  loop

---
+++ 05_loops/three.js
@@ -0,0 +1,41 @@
+//  for of
+
+// ["" ,"" ,""]
+// [{} , {} , {}]
+
+
+// const arr = [1,2,3,4,5,]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const greetings = "Hello world"
+
+// for (const string of greetings) {
+//     console.log(string);
+    
+// }
+
+// Maps
+
+// const map = new Map()
+// map.set('IN' , "india")
+// map.set('USA' , "united states of america")
+// console.log(map);
+
+// for (const [key , value] of map) {
+//     console.log(key , value);
+    
+// }
+
+// const myobj = {
+//     game1: "BGMI",
+//     game2: "COD"
+// }
+
+
+// for( const [key , value] of myobj){
+//     console.log(`${key} :- ${value}`);  
+// }
